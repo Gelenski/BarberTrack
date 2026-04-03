a3c41c4555f664912c54911d496632c8d775163d
Lucas Gelenski <gelenskilucas@gmail.com>
2026-04-03 14:16:19 -0300

    teste de exemplo

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("OK");
-  console.log("TESTE");
+  res.status(200).json({ message: "Hello World!" });
 });
 
 module.exports = app;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-plugin-prettier/recommended";
+import jest from "eslint-plugin-jest";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -8,7 +9,20 @@ export default defineConfig([
   prettier,
   {
     languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["tests/**/*.js", "**/*.test.js", "**/*.spec.js"],
+    ...jest.configs["flat/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
     },
   },
 ]);
