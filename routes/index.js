84c33773b22ce1aabea7cd33047abdd1f0a990f0
Lucas Gelenski <gelenskilucas@gmail.com>
2026-04-08 21:22:34 -0300

    correção de teste para inicio

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
+const path = require("path");
 
-router.use("/", require("./auth"));
+// router.use("/", require("./auth"));
+router.get("/", (req, res) => {
+  res
+    .status(200)
+    .sendFile(path.join(__dirname, "../public/pages/login/index.html"));
+});
 router.use("/cliente", require("./cliente"));
 router.use("/barbearia", require("./barbearia"));
 
--- tests/app.test.js
+++ tests/app.test.js
@@ -2,12 +2,9 @@ const request = require("supertest");
 const app = require("../app");
 
 describe("GET /", () => {
-  it("deve retornar hello world", async () => {
+  it("ok", async () => {
     const response = await request(app).get("/");
 
     expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual({
-      message: "Hello World!",
-    });
   });
 });
