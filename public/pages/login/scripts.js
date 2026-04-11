37f62ab65fd63747e7f9ede3bc2d6e1944cc287f
Adrian <aadrian.s.l10@gmail.com>
2026-04-11 13:48:51 -0300

    corrigido rotas javascript, feito validação front-end no login barbearia

---
+++ public/pages/login/scripts.js
@@ -0,0 +1,42 @@
+ //Elementos DOM
+
+ const formLogin = document.getElementById("form");
+ const emailLogin = document.getElementById("email");
+ const senhaLogin = document.getElementById("senha");
+
+ const btnLogin = document.getElementById("btn-entrar")
+
+ formLogin.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const dadosLogin = {
+        email : emailLogin.value,
+        senha : senhaLogin.value
+
+    }
+
+    try {
+        const resposta = await fetch ("/barbearia/login", {
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(dadosLogin)
+        })
+
+        const resultado = await resposta.json();
+
+        if (resposta.ok) {
+            alert("Login realizado com sucesso")
+
+            
+
+        } else {
+            alert ("Erro no login: " + (resultado.error || "Verifique suas credencias"
+            ))
+        }    
+    }
+    catch (erro) {
+        console.log("Erro na conexão", erro)
+    }
+ })
